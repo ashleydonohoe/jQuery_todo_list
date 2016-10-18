6a54caec3289d5517d25e1b2caa9f8dd3c40b708
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-10-17 23:43:01 -0400

    added ability to delete an item

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+// check off specific todos by clicking
+$("li").click(function() {
+    $(this).toggleClass("completed");
+});
 
+// Delete a to-do
+$("span").click(function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
